a5063e6db75084e1dcc4bdb8b2dcd107c4e8d22f
unknown <qzhsjz@gmail.com>
2019-04-13 20:56:17 +0800

    init commit

---
+++ bot.py
@@ -0,0 +1,7 @@
+import nonebot
+import config
+
+if __name__ == '__main__':
+    nonebot.init(config)
+    nonebot.load_builtin_plugins()
+    nonebot.run()
---
+++ config.py
@@ -0,0 +1,6 @@
+from nonebot.default_config import *
+
+SUPERUSERS = {12345678}
+COMMAND_START = {'', '/', '!', '／', '！'}
+HOST = '127.0.0.1'
+PORT = 5730
