8e91600de510b7bee44656c9c961b8227fdbf333
Angelo Qin <qzhsjz@gmail.com>
2019-04-13 23:14:04 +0800

    add superusers

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from nonebot.default_config import *
 
-SUPERUSERS = {12345678}
+SUPERUSERS = {916327225, 1158395892, 1498109002, 1079703585}
 COMMAND_START = {'', '/', '!', '／', '！'}
 HOST = '127.0.0.1'
 PORT = 5730
