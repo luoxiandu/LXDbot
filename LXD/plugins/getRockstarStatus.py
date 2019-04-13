a065b319064ead105c6551684beb478b6004f54b
Angelo Qin <qzhsjz@gmail.com>
2019-04-13 22:45:42 +0800

    add social club status

--- LXD/plugins/getRockstarStatus.py
+++ LXD/plugins/getRockstarStatus.py
@@ -6,15 +6,19 @@ import json, urllib.request
 async def getRockstarStatus(session:CommandSession):
     result = json.loads(urllib.request.urlopen("https://support.rockstargames.com/services/status.json?tz=Asia/Shanghai").read())
     try:
-        status = {}
+        GTAstatus = {}
+        SCstatus = ''
         for service in result['statuses']:
             if service['tag'] == 'gtao':
                 for platform in service['services_platforms']:
-                    status[platform['name']] = '服务正常' if platform['service_status_id'] == 1 else '暂不可用，Rockstar内部错误代码为' + platform['service_status_id']
+                    GTAstatus[platform['name']] = '服务正常' if platform['service_status_id'] == 1 else '暂不可用，Rockstar内部错误代码为' + platform['service_status_id']
+            if service['tag'] == 'sc':
+                SCstatus = '服务正常' if service['services_platforms'][0]['service_status_id'] == 1 else '暂不可用，Rockstar内部错误代码为' + service['services_platforms'][0]['service_status_id']
     except KeyError:
         await session.send("查询失败，请重试！")
         return
     rstr = "根据Rockstar官方通报，目前GTA Online各平台服务器状态如下：\n"
-    for platform in status:
-        rstr += platform + " 平台：" + status[platform] + "\n"
+    for platform in GTAstatus:
+        rstr += platform + " 平台：" + GTAstatus[platform] + "\n"
+    rstr += 'Rockstar Social Club 服务状态：' + SCstatus
     await session.send(rstr)
