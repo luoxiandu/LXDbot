fc5f3c311b1087ef4052fc7748cebd7738518d0b
Angelo Qin <qzhsjz@gmail.com>
2019-04-14 18:46:39 +0800

    change Rockstarstatus Response

--- LXD/plugins/getRockstarStatus.py
+++ LXD/plugins/getRockstarStatus.py
@@ -15,10 +15,10 @@ async def getRockstarStatus(session:CommandSession):
             if service['tag'] == 'sc':
                 SCstatus = '服务正常' if service['services_platforms'][0]['service_status_id'] == 1 else '暂不可用，Rockstar内部错误代码为' + service['services_platforms'][0]['service_status_id']
     except KeyError:
-        await session.send("查询失败，请重试！")
+        await session.finish("查询失败，请重试！")
         return
     rstr = "根据Rockstar官方通报，目前GTA Online各平台服务器状态如下：\n"
     for platform in GTAstatus:
         rstr += platform + " 平台：" + GTAstatus[platform] + "\n"
     rstr += 'Rockstar Social Club 服务状态：' + SCstatus
-    await session.send(rstr)
+    await session.finish(rstr)
