c57b40f6e3bb3f45775c99add05d39762f3c35c9
Angelo Qin <qzhsjz@gmail.com>
2019-04-15 21:31:30 +0800

    manual deposit

--- LXD/plugins/AlipayClient.py
+++ LXD/plugins/AlipayClient.py
@@ -1,6 +1,8 @@
 from nonebot import on_command, CommandSession
 from LXD.services.AlipaySvr import AlipaySvr
 from LXD.services.DBSvr import DB
+from nonebot.permission import SUPERUSER
+from importlib import reload
 
 alipay = AlipaySvr()
 db = DB()
@@ -30,4 +32,35 @@ async def checkAlipay(session:CommandSession):
             await session.finish(repr(result) + '元已经收到，充值成功！')
         else:
             await session.finish('未查询到款项！请确认钱款已经打入正确的账户。')
+
+@on_command('reboot', aliases=('重启支付宝模块',), privileged=SUPERUSER)
+async def reboot(session:CommandSession):
+    reload(AlipaySvr)
+    await session.finish('模块重新加载完毕')
+
+@on_command('manualAlipayCheck', aliases=('手动支付宝充值',), privileged=SUPERUSER, shell_like=True)
+async def manualAlipayCheck(session:CommandSession):
+    try:
+        user = session.argv[0]
+    except KeyError:
+        await session.send('第一个参数是被充值QQ号！')
+        return
+    try:
+        tradeNo = session.argv[1]
+    except KeyError:
+        await session.send('第二个参数是支付宝流水号！')
+        return
+    try:
+        amount = session.argv[2]
+    except KeyError:
+        await session.send('第三个参数是金额！')
+        return
+    db.saveAlipayTradeNo({
+        'tradeNo': tradeNo,
+        'memo': '#手动充值#' + user,
+        'amount': int(float(amount) * 100)
+    })
+    db.useAlipayTradeNo(tradeNo)
+    db.deposit(user, int(float(amount) * 100))
+    await session.finish('充值完毕！为用户' + user + '充值' + amount)
 
