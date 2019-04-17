30008f4f1d913bb508d0e3922a2e12a5b7f3f3fc
Angelo Qin <qzhsjz@gmail.com>
2019-04-17 14:19:59 +0800

    modify ForthPartyPayment Function

--- LXD/services/ForthPartyPaySvr.py
+++ LXD/services/ForthPartyPaySvr.py
@@ -1,8 +1,6 @@
 import nonebot
-from aiocqhttp.exceptions import ActionFailed
 from LXD.services.DBSvr import DB
 import aiohttp
-from aiohttp import web
 import config
 import hashlib
 import json
@@ -10,14 +8,10 @@ import base64
 import time
 
 class ForthPaySvr:
-    webserver = None
     __db__ = None
     __QQbot__ = nonebot.get_bot()
 
     def __init__(self):
-        self.webserver = web.Application()
-        self.webserver.add_routes([web.post('/020pay', self.notify_handler_020)])
-        web.run_app(self.webserver)
         self.__db__ = DB()
 
     async def getPayQRcode(self, price, type, orderuid, goodsname):
@@ -43,16 +37,3 @@ class ForthPaySvr:
                             return base64.b64encode(resp.read())
                 else:
                     return None
-
-    async def notify_handler_020(self, request):
-        data = await request.post()
-        kstr = data['actual_price'] + data['bill_no'] + data['orderid'] + data['orderuid'] + data['price'] + config.pay_token
-        if hashlib.md5(kstr.encode('utf-8')).hexdigest() == data['key']:
-            self.__db__.saveForthPartyOrder(data)
-            self.__db__.deposit(data['orderuid'], int(data['price']))
-            try:
-                await self.__QQbot__.send_private_msg_rate_limited(user_id=int(data['orderuid']), message='您的' + repr(float(data['price']) / 100) + '元充值已到账！')
-            except ActionFailed as e:
-                print('酷QHTTP插件错误，返回值：' + repr(e.retcode))
-        else:
-            return web.Response(text='Token Validation Failed.')
