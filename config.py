0c0998bcc04a7f1df8bcc986827259775d5e47a7
Angelo Qin <qzhsjz@gmail.com>
2019-04-17 15:43:25 +0800

    modify host

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from nonebot.default_config import *
 
 SUPERUSERS = {916327225, 1158395892, 1498109002, 1079703585}
 COMMAND_START = {'', '/', '!', '／', '！'}
-HOST = '127.0.0.1'
+HOST = '0.0.0.0'
 PORT = 5730
 
 pay_identification = '9B8XY9K6MB16527K'
