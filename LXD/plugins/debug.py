d176e10a11147e594e83d0013c6126a63ec8103b
Angelo Qin <qzhsjz@gmail.com>
2019-04-18 16:20:54 +0800

    add debug plugin

---
+++ LXD/plugins/debug.py
@@ -0,0 +1,9 @@
+import nonebot
+from nonebot import on_command, CommandSession
+
+
+@on_command('getPlugins', aliases='插件状态', only_to_me=False)
+async def getPlugins(session:CommandSession):
+    plugins = nonebot.plugin.get_loaded_plugins()
+    await session.send('我现在支持以下功能：\n\n' +
+                       '\n'.join(map(lambda p: p.name, filter(lambda p: p.name, plugins))))
