40fe2b320c97acfa6aee6cb7cdf4826c92ac5483
Angelo Qin <qzhsjz@gmail.com>
2019-04-18 16:26:00 +0800

    why this can be debug

--- LXD/plugins/debug.py
+++ LXD/plugins/debug.py
@@ -5,5 +5,6 @@ from nonebot import on_command, CommandSession
 @on_command('getPlugins', aliases=('插件状态', ), only_to_me=False)
 async def getPlugins(session:CommandSession):
     plugins = nonebot.plugin.get_loaded_plugins()
+    print(plugins)
     await session.send('我现在支持以下功能：\n\n' +
                        '\n'.join(map(lambda p: p.name, filter(lambda p: p.name, plugins))))
