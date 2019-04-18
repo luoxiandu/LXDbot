50ee76cbc00214db3f2fd19d19fed5e5312956b5
Angelo Qin <qzhsjz@gmail.com>
2019-04-18 16:30:16 +0800

    debug debug

--- LXD/plugins/debug.py
+++ LXD/plugins/debug.py
@@ -7,4 +7,4 @@ async def getPlugins(session:CommandSession):
     plugins = nonebot.plugin.get_loaded_plugins()
     print(plugins)
     await session.send('我现在支持以下功能：\n\n' +
-                       '\n'.join(map(lambda p: p.name, filter(lambda p: p.name, plugins))))
+                       '\n'.join(map(lambda p: p.module, filter(lambda p: p.module, plugins))))
