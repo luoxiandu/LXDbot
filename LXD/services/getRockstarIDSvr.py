8506d160cfbeb38725fc080b3749c0cb60b585b2
Angelo Qin <qzhsjz@gmail.com>
2019-04-19 13:25:42 +0800

    add Chrome Binary

--- LXD/services/getRockstarIDSvr.py
+++ LXD/services/getRockstarIDSvr.py
@@ -9,7 +9,7 @@ class RockstarIDgetter:
     def __init__(self):
         caps = DesiredCapabilities.CHROME
         caps['loggingPrefs'] = {'performance': 'ALL'}
-        self.__browser__ = webdriver.Chrome(desired_capabilities=caps)
+        self.__browser__ = webdriver.Chrome(desired_capabilities=caps, executable_path='bin/Chrome/chromedriver.exe')
         self.__browser__.get("https://zh-cn.socialclub.rockstargames.com")
         logs = [json.loads(log['message'])['message'] for log in self.__browser__.get_log('performance')]
         print(logs)
