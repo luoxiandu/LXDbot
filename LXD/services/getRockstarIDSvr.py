0ada6da2b5455ddeff538e0e2e76c510257fd437
Angelo Qin <qzhsjz@gmail.com>
2019-04-19 14:18:28 +0800

    test for har getting

--- LXD/services/getRockstarIDSvr.py
+++ LXD/services/getRockstarIDSvr.py
@@ -11,5 +11,16 @@ class RockstarIDgetter:
         caps['loggingPrefs'] = {'performance': 'ALL'}
         self.__browser__ = webdriver.Chrome(desired_capabilities=caps, executable_path='bin/Chrome/chromedriver.exe')
         self.__browser__.get("https://zh-cn.socialclub.rockstargames.com")
+        # logs = [json.loads(log['message'])['message'] for log in self.__browser__.get_log('performance')]
+        self.__browser__.implicitly_wait(120)
+        avatar = self.__browser__.find_element_by_xpath("//div[@data-ui-name='avatar']")
+        if avatar:
+            print('登录成功！')
+        self.getID('AngeloTheCat')
+
+    def getID(self, username):
+        self.__browser__.get("https://zh-cn.socialclub.rockstargames.com/member/%s/" % username)
+        self.__browser__.implicitly_wait(10)
         logs = [json.loads(log['message'])['message'] for log in self.__browser__.get_log('performance')]
-        print(logs)
+        with open('har.json', 'w') as fp:
+            json.dump(logs, fp)
