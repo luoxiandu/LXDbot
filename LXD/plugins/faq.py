357f012e8f026dc71df89c11a22f60ee140c5a5e
Angelo Qin <qzhsjz@gmail.com>
2019-04-20 09:38:01 +0800

    add private question

--- LXD/plugins/faq.py
+++ LXD/plugins/faq.py
@@ -11,7 +11,14 @@ db = DB()
 async def ask(session:CommandSession):
     question = session.current_arg
     answer = db.getvar('question_' + question)
-    session.finish(answer)
+    if answer:
+        session.finish(answer)
+    else:
+        answer = db.getvar('question_private_' + question)
+        if answer:
+            await session.send(answer, ensure_private=True)
+            session.finish()
+    session.finish('没有找到您问的问题 ' + question + ' 哦')
 
 
 @on_command('addQuestion', aliases=('设置问题', '设问'), permission=SUPERUSER)
@@ -20,4 +27,13 @@ async def addQuestion(session:CommandSession):
     question = c[1].strip('#')
     answer = c[2].strip()
     db.setvar('question_' + question, answer)
+    session.finish('问题' + question + '设置成功！')
+
+
+@on_command('addPrivateQuestion', aliases=('设置私密问题', '私密设问'), permission=SUPERUSER)
+async def addPrivateQuestion(session:CommandSession):
+    c = re.split(r'(#[^#]+#)(.+)', session.current_arg, flags=re.M | re.S)
+    question = c[1].strip('#')
+    answer = c[2].strip()
+    db.setvar('question_private_' + question, answer)
     session.finish('问题' + question + '设置成功！')
