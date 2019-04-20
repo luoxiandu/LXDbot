29410d00d39639e054e4e6c735f5208ece6f6dbf
Angelo Qin <qzhsjz@gmail.com>
2019-04-20 09:24:51 +0800

    add notice request handler

--- LXD/plugins/noticeHelper.py
+++ LXD/plugins/noticeHelper.py
@@ -1,4 +1,5 @@
-from nonebot import on_notice, NoticeSession, logger
+from nonebot import on_notice, on_request, NoticeSession, RequestSession
+from nonebot.log import logger
 
 __plugin_name__ = 'LXD.noticeHelper'
 
@@ -6,6 +7,11 @@ __plugin_name__ = 'LXD.noticeHelper'
 @on_notice
 async def _(session: NoticeSession):
     logger.info('有新的通知事件：%s', session.ctx)
+
+
+@on_request
+async def _(session: RequestSession):
+    logger.info('有新的请求事件：%s', session.ctx)
 
 # 将函数注册为群成员增加通知处理器
 @on_notice('group_increase')
