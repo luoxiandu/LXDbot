976914a8e9dc32c22d8b0442356e9e1f61e9f929
Angelo Qin <qzhsjz@gmail.com>
2019-04-23 18:48:24 +0800

    add getdlllist

--- LXD/plugins/DLLHelper.py
+++ LXD/plugins/DLLHelper.py
@@ -12,3 +12,8 @@ bot = nonebot.get_bot()
 async def getdll():
     data = await request.form
     return json.dumps({'payload': 'ZnVja2l0', 'sessionkey': 'ABCD456'})
+
+@bot.server_app.route('/getdlllist', methods=['POST'])
+async def getdlllist():
+    data = await request.form
+    return json.dumps({'payload': [{'id': 1, 'name': '外挂1'}, {'id': 2, 'name': '外挂2'}], 'sessionkey': 'ABCD567'})
