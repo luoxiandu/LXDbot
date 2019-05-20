b024b9ff843689d1aaa6d56e5906b406cb3b6bb6
Angelo Qin <qzhsjz@gmail.com>
2019-05-20 16:11:09 +0800

    modify sequence

--- LXD/plugins/GameAccountService.py
+++ LXD/plugins/GameAccountService.py
@@ -40,9 +40,9 @@ async def buygameservice(session:CommandSession):
     tigershark = cash // 200000
     cash -= tigershark * 200000
     redshark = cash // 100000
-    await session.send("根据您的需求，为您定制方案：巨齿鲨卡%d张，鲸鲨卡%d张，大白鲨卡%d张，牛鲨卡%d张，虎鲨卡%d张，红鲨卡%d张" % (megalodon, whale, greatwhite, bullshark, tigershark, redshark))
     level = session.get('level', prompt='请输入您期望更改的级别（如果不购买等级更改请输入0）：')
     unlock = session.get('unlock', prompt='您要购买解锁吗？如购买解锁请回复“解锁”，否则输入任意内容继续：')
+    await session.send("根据您的需求，为您定制鲨鱼卡方案：巨齿鲨卡%d张，鲸鲨卡%d张，大白鲨卡%d张，牛鲨卡%d张，虎鲨卡%d张，红鲨卡%d张" % (megalodon, whale, greatwhite, bullshark, tigershark, redshark))
     total = 0
     total += megalodon * int(db.getprice('megalodon_' + grpid))
     total += whale * int(db.getprice('whale_' + grpid))
