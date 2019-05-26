b90550d6efb8cef996029674968fb3613a6a6225
Angelo Qin <qzhsjz@gmail.com>
2019-05-26 20:26:46 +0800

    fix wrong logic

--- LXD/plugins/account.py
+++ LXD/plugins/account.py
@@ -74,7 +74,7 @@ async def triallogin():
     IP = request.remote_addr
     HWID = data['HWID']
     ret = {}
-    if HWID and IP and data['version'] == '1.2' and db.chktrial(HWID) or True:
+    if HWID and IP and data['version'] == '1.2' and (db.chktrial(HWID) or True):
         db.newtrial(HWID, IP)
         db.varpp('logincount')
         db.varpp('logincountday')
