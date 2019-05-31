49f086203b969544a9f637626efc8059b99572e1
Angelo Qin <qzhsjz@gmail.com>
2019-06-01 02:55:26 +0800

    make bot asgi

--- bot.py
+++ bot.py
@@ -2,11 +2,14 @@ from os import path
 import nonebot
 import config
 
+nonebot.init(config)
+nonebot.load_builtin_plugins()
+nonebot.load_plugins(
+    path.join(path.dirname(__file__), 'LXD', 'plugins'),
+    'LXD.plugins'
+)
+bot = nonebot.get_bot()
+app = bot.asgi
+
 if __name__ == '__main__':
-    nonebot.init(config)
-    nonebot.load_builtin_plugins()
-    nonebot.load_plugins(
-        path.join(path.dirname(__file__), 'LXD', 'plugins'),
-        'LXD.plugins'
-    )
     nonebot.run()
