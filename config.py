2ea30cb12861d08f749ea2f08e69bed11838a0da
Angelo Qin <qzhsjz@gmail.com>
2019-06-07 00:55:32 +0800

    remove ip and port

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ from nonebot.default_config import *
 
 SUPERUSERS = {916327225, 1158395892, 1498109002, 1079703585}
 COMMAND_START = {'', '/', '!', '／', '！'}
-HOST = '0.0.0.0'
-PORT = 5730
+# HOST = '0.0.0.0'
+# PORT = 5730
 
 pay_identification = '9B8XY9K6MB16527K'
 pay_token = 'NIF7DTRGQ7O389T8YP46RSHQODZ886O9'
